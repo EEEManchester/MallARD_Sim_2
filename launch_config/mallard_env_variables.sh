90ddbe1c69a0d20391e1f68c29df9e3562441f41
IamGyal <hangyeolgo@gmail.com>
2021-11-03 14:34:10 +0000

    readme, env vars

--- launch_config/mallard_env_variables.sh
+++ launch_config/mallard_env_variables.sh
@@ -4,8 +4,8 @@
 # Include this file in .bashrc to source it
 # source ~/mallard_ws/src/MallARD/launch_config/mallard_env_variables.sh
 
-export MALLARD_REAL=1            # 0: Simulation Mode. 1: Real Robot Mode. 
+export MALLARD_REAL=0            # 0: Simulation Mode. 1: Real Robot Mode. 
 export MALLARD_COMP=1            # 0: Robot computer. 1: Base Staion comptuer. 2: another comptuer. Only relavant for Real Robot Mode.
-export MALLARD_AUTONOMOUS=1      # 0: Manual Mode. 1: Autonomous Mode. 
+export MALLARD_AUTONOMOUS=0      # 0: Manual Mode. 1: Autonomous Mode. 
 
-export MALLARD_VERSION='001_EL' # The version of MallARD. E.g. 001_SIM, 002_SIM, 003_SIM, 001_REAL, 002_REAL, 003_REAL, 001_EL.
+export MALLARD_VERSION='001_SIM' # The version of MallARD. E.g. 001_SIM, 002_SIM, 003_SIM, 001_REAL, 002_REAL, 003_REAL, 001_EL.
